5ae06b7915996194ce63846ddfe9b0bd940f6a75
Marcio <Marcio@192.168.221.128>
2014-09-06 16:28:44 -0300

    Merge origin/master
    Conflicts:
    	HelloWebJSPMaven/src/main/webapp/META-INF/context.xml

---
+++ MonitoraNuvem/src/main/java/br/com/monitoranuvem/connection/Credentials.java
@@ -0,0 +1,27 @@
+package br.com.monitoranuvem.connection;
+
+/**
+ *
+ * @author Marcio
+ */
+public class Credentials {
+    private String login;
+    private String password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
