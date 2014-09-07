aa9f80c1b3555572a32584c60b4151bd7720527e
Marcio <Marcio@192.168.221.128>
2014-09-07 20:17:37 -0300

    Merge origin/master
    Conflicts:
    	HelloWebJSPMaven/src/main/webapp/META-INF/context.xml

--- MonitoraNuvem/src/main/java/br/com/monitoranuvem/controller/ProviderDialogControl.java
+++ MonitoraNuvem/src/main/java/br/com/monitoranuvem/controller/ProviderDialogControl.java
@@ -1,27 +1,8 @@
 package br.com.monitoranuvem.controller;
 
-import br.com.monitoranuvem.model.Configuration;
-import br.com.monitoranuvem.model.Credentials;
 import br.com.monitoranuvem.model.MNComputeServiceContextFactory;
-import br.com.monitoranuvem.model.Provider;
-import java.util.ArrayList;
-import org.jclouds.cloudwatch.CloudWatch;
-import org.jclouds.cloudwatch.domain.Dimension;
-import org.jclouds.cloudwatch.domain.EC2Constants;
-import org.jclouds.cloudwatch.domain.Namespaces;
-import org.jclouds.cloudwatch.options.ListMetricsOptions;
 import org.jclouds.compute.ComputeService;
 import org.jclouds.compute.ComputeServiceContext;
-import org.jclouds.compute.domain.ComputeMetadata;
-import org.jclouds.compute.domain.NodeMetadata;
-import org.jclouds.domain.Location;
-import org.jclouds.domain.LocationScope;
-
-
-
-
-
-
 
 /**
  *
@@ -29,69 +10,9 @@ import org.jclouds.domain.LocationScope;
  */
 public class ProviderDialogControl {
 
-    public ProviderDialogControl() {
-
-    }
-
-    public boolean connecta(Provider p, String login, String password) {
-        Credentials cred = new Credentials();
-        cred.setAcessKey(login);
-        cred.setSecretKey(password);
-        Configuration.getInstance().setProvider(p);
-        Configuration.getInstance().setCredentials(cred);
-        listaCServ();
-        return true;
-    }
-//
-////    public void listaBlob() {
-////        BlobStoreContext context = JVBlobStoreContextFactory.createContext();
-////        BlobStore blobStore = context.getBlobStore();
-////        DefaultListModel list = new DefaultListModel();
-////        
-////        for (StorageMetadata storage : blobStore.list()) {
-////            System.out.println(storage.getName());
-////            list.addElement(storage);
-////        }
-////    }
-
-    public void listaCServ() {
+    public ComputeService getListaCServ() {
         ComputeServiceContext context = MNComputeServiceContextFactory.createContext();
         ComputeService compute = context.getComputeService();
-        ArrayList<ComputeMetadata> metadatas = new ArrayList<ComputeMetadata>();
-
-        for (ComputeMetadata node : compute.listNodes()) {
-            metadatas.add(node);
-            NodeMetadata metadata = compute.getNodeMetadata(node.getId());
-            System.out.println(metadata.getName());
-            System.out.println(metadata.getLocation().getId());
-            System.out.println(metadata.getProviderId());
-
-            CloudWatch cw = new CloudWatch();
-            Dimension instanceIdDimension = new Dimension(EC2Constants.Dimension.INSTANCE_ID, node.getId());
-            ListMetricsOptions lmOptions = ListMetricsOptions.builder()
-                    .metricName(EC2Constants.MetricName.CPU_UTILIZATION)
-                    .namespace(Namespaces.EC2)
-                    .dimension(instanceIdDimension)
-                    .build();
-
-            String region = getRegion(node.getLocation());
-         }
-    }
-
-    private static String getRegion(Location location) {
-        // Just to be safe
-        if (location == null) {
-            return null;
-        }
-
-        String region = null;
-        while (region == null && location.getParent() != null) {
-            if (location.getScope() == LocationScope.REGION) {
-                region = location.getId();
-            } else {
-                location = location.getParent();
-            }
-        }
-        return region;
+        return compute;
     }
 }
