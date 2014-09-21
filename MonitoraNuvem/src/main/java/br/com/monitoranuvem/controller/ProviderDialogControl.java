0bd0d9343298e7a24c6b6b1bd0f484fcbe58d59b
Marcio <Marcio@192.168.221.128>
2014-09-20 21:04:56 -0300

    te4ste

--- MonitoraNuvem/src/main/java/br/com/monitoranuvem/controller/ProviderDialogControl.java
+++ MonitoraNuvem/src/main/java/br/com/monitoranuvem/controller/ProviderDialogControl.java
@@ -1,13 +1,7 @@
 package br.com.monitoranuvem.controller;
 
-import br.com.monitoranuvem.model.MNBlobStoreContext;
 import br.com.monitoranuvem.model.MNComputeServiceContext;
-import br.com.monitoranuvem.model.Provider;
 import br.com.monitoranuvem.model.ProviderService;
-import br.com.monitoranuvem.model.ProviderServiceBD;
-import java.sql.SQLException;
-import org.jclouds.blobstore.BlobStore;
-import org.jclouds.blobstore.BlobStoreContext;
 import org.jclouds.compute.ComputeService;
 import org.jclouds.compute.ComputeServiceContext;
 
