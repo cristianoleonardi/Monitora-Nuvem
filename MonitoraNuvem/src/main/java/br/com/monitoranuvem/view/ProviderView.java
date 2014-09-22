0568b16bff295d016b6f85a1097260f2f13e1ab3
cristianoleonardi <cristianoleonardi@hotmail.com>
2014-09-21 22:57:49 -0300

    CRUD Provedores FINAL.

--- MonitoraNuvem/src/main/java/br/com/monitoranuvem/view/ProviderView.java
+++ MonitoraNuvem/src/main/java/br/com/monitoranuvem/view/ProviderView.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpSession;
 
 /**
  *
- * @author Cristiano
+ * @author Cristiano Leonardi, Márcio Bolzan
  */
 public class ProviderView extends HttpServlet {
 
@@ -35,14 +35,14 @@ public class ProviderView extends HttpServlet {
         //Instancia do controller do provider
         ProviderControl pc = new ProviderControl();
 
-        //Instancia para manipular variáveis de sessao
+        //Instancia a sessão para manipular as variáveis de sessao
         HttpSession session = request.getSession(true);
 
         //Ação que determina o que será executado pelo Controller.
         String action = request.getParameter("action");
 
-        if (action == null || action.equalsIgnoreCase("")) {
-            //Lista provedores cadastrados
+        if (action == null || action.equalsIgnoreCase("") || action.equalsIgnoreCase("listarProvider")) {
+            //###LISTAR PROVIDER###
             ArrayList<Provider> listaProvedores = pc.listaProvider();
 
             session.setAttribute("listaProvedores", listaProvedores);
@@ -50,54 +50,88 @@ public class ProviderView extends HttpServlet {
             RequestDispatcher rd = request
                     .getRequestDispatcher("/provider.jsp");
             rd.forward(request, response);
+
         } else if (action.equalsIgnoreCase("criarProvider")) {
-            //Cadastra novo provedor
+            //###CRIAR PROVIDER###
             String provider = request.getParameter("provider");
 
             if (pc.criarProvider(provider)) {
+                session.setAttribute("responseAction", "Ok");
+                session.setAttribute("responseMsg", "<strong><i class=\"icon24 i-checkmark-circle\"></i> Parabéns!</strong> Seu provedor foi cadastrado com sucesso.");
 
-                session.setAttribute("responseAction", "criarOk");
+                ArrayList<Provider> listaProvedores = pc.listaProvider();
+                session.setAttribute("listaProvedores", listaProvedores);
 
                 RequestDispatcher rd = request
                         .getRequestDispatcher("/provider.jsp");
                 rd.forward(request, response);
             } else {
-                session.setAttribute("responseAction", "criarErro");
+                session.setAttribute("responseAction", "Erro");
+                session.setAttribute("responseMsg", "<strong><i class=\"icon24 i-close-4\"></i> Erro!</strong> Não foi possível cadastrar este provedor.");
 
                 RequestDispatcher rd = request
                         .getRequestDispatcher("/provider.jsp");
                 rd.forward(request, response);
             }
-        } else if (action.equalsIgnoreCase("atualizarProvider")) {
-            //Altera um provedor cadastrado
 
-            RequestDispatcher rd = request
-                    .getRequestDispatcher("/provider.jsp");
-            rd.forward(request, response);
-        } else if (action.equalsIgnoreCase("listarProvider")) {
-            //Listar um provedor cadastrado para alteração
+        } else if (action.equalsIgnoreCase("buscarProvider")) {
+            //###BUSCAR PROVIDER PARA ATUALIZAR O MESMO###
             String id = request.getParameter("id");
-            
+
             Provider provedor = new Provider();
             provedor = pc.buscaProvider(Integer.parseInt(id));
-            
+
             session.setAttribute("provedor", provedor);
             session.setAttribute("action", "atualizarProvider");
-            
+
             RequestDispatcher rd = request
                     .getRequestDispatcher("/provider.jsp");
             rd.forward(request, response);
-        } else if (action.equalsIgnoreCase("deletarProvider")) {
-            //Cadastra novo provedor
-            String id = request.getParameter("id");
-            if (pc.deletaProvider(Integer.parseInt(id))) {
-                session.setAttribute("responseAction", "deletarOk");
 
+        } else if (action.equalsIgnoreCase("atualizarProvider")) {
+            //###ALTERAR PROVIDER###
+            String id = request.getParameter("id");
+            String provider = request.getParameter("provider");
+
+            if (pc.atualizaProvider(Integer.parseInt(id), provider)) {
+                session.setAttribute("responseAction", "Ok");
+                session.setAttribute("responseMsg", "<strong><i class=\"icon24 i-checkmark-circle\"></i> Parabéns!</strong> Seu provedor foi atualizado com sucesso.");
+
+                ArrayList<Provider> listaProvedores = pc.listaProvider();
+                session.setAttribute("listaProvedores", listaProvedores);
+                
                 RequestDispatcher rd = request
                         .getRequestDispatcher("/provider.jsp");
                 rd.forward(request, response);
             } else {
-                session.setAttribute("responseAction", "deletarErro");
+                session.setAttribute("responseAction", "Erro");
+                session.setAttribute("responseMsg", "<strong><i class=\"icon24 i-close-4\"></i> Erro!</strong> Não foi possível atualizar este provedor.");
+
+                RequestDispatcher rd = request
+                        .getRequestDispatcher("/provider.jsp");
+                rd.forward(request, response);
+            }
+
+            RequestDispatcher rd = request
+                    .getRequestDispatcher("/provider.jsp");
+            rd.forward(request, response);
+
+        } else if (action.equalsIgnoreCase("deletarProvider")) {
+            //###DELETAR PROVIDER###
+            String id = request.getParameter("id");
+            if (pc.deletaProvider(Integer.parseInt(id))) {
+                session.setAttribute("responseAction", "Ok");
+                session.setAttribute("responseMsg", "<strong><i class=\"icon24 i-checkmark-circle\"></i> Parabéns!</strong> Seu provedor removido com sucesso.");
+
+                ArrayList<Provider> listaProvedores = pc.listaProvider();
+                session.setAttribute("listaProvedores", listaProvedores);
+                
+                RequestDispatcher rd = request
+                        .getRequestDispatcher("/provider.jsp");
+                rd.forward(request, response);
+            } else {
+                session.setAttribute("responseAction", "Erro");
+                session.setAttribute("responseMsg", "<strong><i class=\"icon24 i-close-4\"></i> Erro!</strong> Não foi possível remover este provedor.");
 
                 RequestDispatcher rd = request
                         .getRequestDispatcher("/provider.jsp");
@@ -106,60 +140,56 @@ public class ProviderView extends HttpServlet {
         }
     }
 
-        // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-        /**
-         * Handles the HTTP <code>GET</code> method.
-         *
-         * @param request servlet request
-         * @param response servlet response
-         * @throws ServletException if a servlet-specific error occurs
-         * @throws IOException if an I/O error occurs
-         */
-        @Override
-        protected void doGet
-        (HttpServletRequest request, HttpServletResponse response)
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            try {
-                processRequest(request, response);
-            } catch (ClassNotFoundException ex) {
-                Logger.getLogger(ProviderView.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (SQLException ex) {
-                Logger.getLogger(ProviderView.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ProviderView.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProviderView.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        /**
-         * Handles the HTTP <code>POST</code> method.
-         *
-         * @param request servlet request
-         * @param response servlet response
-         * @throws ServletException if a servlet-specific error occurs
-         * @throws IOException if an I/O error occurs
-         */
-        @Override
-        protected void doPost
-        (HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-            try {
-                processRequest(request, response);
-            } catch (ClassNotFoundException ex) {
-                Logger.getLogger(ProviderView.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (SQLException ex) {
-                Logger.getLogger(ProviderView.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-
-        /**
-         * Returns a short description of the servlet.
-         *
-         * @return a String containing servlet description
-         */
-        @Override
-        public String getServletInfo
-        
-            () {
-        return "Short description";
-        }// </editor-fold>
 
     }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ProviderView.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProviderView.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- MonitoraNuvem/src/main/webapp/provider.jsp
+++ MonitoraNuvem/src/main/webapp/provider.jsp
@@ -27,18 +27,20 @@
                     <h1><i class="icon20 i-cloud-download"></i> Cadastro de Provedores</h1>
                 </div>
 
-                <% if (session.getAttribute("responseAction") == "criarOk") { %>
+                <% if (session.getAttribute("responseAction") == "Ok") { %>
                 <div class="alert alert-success">
                     <button type="button" class="close" data-dismiss="alert">&times;</button>
-                    <strong><i class="icon24 i-checkmark-circle"></i> Parabéns!</strong> Seu provedor foi gravado com sucesso.
+                    <% out.print(session.getAttribute("responseMsg")); %>
                 </div>
                 <% session.removeAttribute("responseAction"); %>
-                <% } else if (session.getAttribute("responseAction") == "criarErro") { %>
+                <% session.removeAttribute("responseMsg"); %>
+                <% } else if (session.getAttribute("responseAction") == "Erro") { %>
                 <div class="alert alert-error">
                     <button type="button" class="close" data-dismiss="alert">&times;</button>
-                    <strong><i class="icon24 i-close-4"></i> Erro!</strong> Não foi possível cadastrar este provedor.
+                    <% out.print(session.getAttribute("responseMsg")); %>
                 </div>
                 <% session.removeAttribute("responseAction"); %>
+                <% session.removeAttribute("responseMsg"); %>
                 <% } %>
 
                 <div class="row">
@@ -53,27 +55,37 @@
 
                             <div class="panel-body">
                                 <form class="form-horizontal" action="provider" method="POST">
-                                    <% Provider provedor = (Provider)session.getAttribute("provedor"); %>
+                                    <% Provider provedor = (Provider) session.getAttribute("provedor"); %>
                                     <% if (session.getAttribute("action") == "atualizarProvider") { %>
                                     <div class="form-group">
                                         <label class="col-lg-2 control-label" for="normal">ID</label>
                                         <div class="col-lg-10">
-                                            <input class="form-control" type="text" name="provider" value="<% if(provedor != null) out.print(provedor.getId()); %>" required readonly /><br />
+                                            <input class="form-control" type="text" name="id" value="<% if (provedor != null) {
+                                                    out.print(provedor.getId());
+                                                } %>" required readonly /><br />
                                         </div>
                                     </div>
                                     <% } %>
                                     <div class="form-group">
                                         <label class="col-lg-2 control-label" for="normal">Provedor</label>
-                                        <div class="col-lg-10">
-                                            <input class="form-control" type="text" name="provider" value="<% if(provedor != null) out.print(provedor.getNome()); %>" required /><br />
+                                        <div class="col-lg-5">
+                                            <input class="form-control" type="text" name="provider" value="<% if (provedor != null) {
+                                                    out.print(provedor.getNome());
+                                                } %>" required maxlength="45" /><br />
                                         </div>
                                     </div>
+                                    <% session.removeAttribute("provedor"); %>
                                     <div class="form-group">
                                         <div class="col-lg-offset-2">
                                             <div class="pad-left15">
+                                                <% if (session.getAttribute("action") == "atualizarProvider") { %>
+                                                <input type="hidden" name="action" value="atualizarProvider" />
+                                                <% } else { %>
                                                 <input type="hidden" name="action" value="criarProvider" />
-                                                <button type="submit" class="btn btn-success btn-xs"><i class="icon10 i-database"> Cadastrar</i></button>
-                                                <button type="reset" class="btn btn-danger btn-xs"><i class="icon10 i-close"> Cancelar</i></button>
+                                                <% } %>
+                                                <button type="submit" class="btn btn-success btn-xs"><i class="icon10 i-database"> <% if (session.getAttribute("action") == "atualizarProvider") out.print("Atualizar"); else out.print("Cadastrar"); %></i></button>
+                                                <a href="provider" class="btn btn-danger btn-xs"><i class="icon10 i-close"> Cancelar</i></a>
+                                                <% session.removeAttribute("action"); %>
                                             </div>
                                         </div>
                                     </div>
@@ -113,7 +125,7 @@
                                             <td class="center">
                                                 <form action="provider" method="POST">
                                                     <input type="hidden" name="id" value="<% out.print(provedores.getId()); %>" />
-                                                    <input type="hidden" name="action" value="listarProvider" />
+                                                    <input type="hidden" name="action" value="buscarProvider" />
                                                     <button type="submit" class="btn btn-primary btn-xs" title="Editar"><i class="icon10 i-pencil"> Editar</i></button>&nbsp;
                                                 </form>
                                             </td>
