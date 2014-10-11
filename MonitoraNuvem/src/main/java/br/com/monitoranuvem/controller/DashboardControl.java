f851d79a6862b2a3400e04d7cc85ab827867887a
Marcio <Marcio@Marcio-PC>
2014-10-11 20:15:07 -0300

    teste

--- MonitoraNuvem/src/main/java/br/com/monitoranuvem/controller/DashboardControl.java
+++ MonitoraNuvem/src/main/java/br/com/monitoranuvem/controller/DashboardControl.java
@@ -2,11 +2,13 @@ package br.com.monitoranuvem.controller;
 
 import br.com.monitoranuvem.model.Provider;
 import br.com.monitoranuvem.model.ProviderBD;
+import br.com.monitoranuvem.model.ThreadAmazon;
 import br.com.monitoranuvem.model.ThreadOpenStack;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -21,20 +23,30 @@ public class DashboardControl {
     }
 
     public void monitoraNuvem() throws ClassNotFoundException, SQLException, ParseException {
-        //       ArrayList<Provider> list = ;
         executor = Executors.newFixedThreadPool(new ProviderBD().listaProvider().size());
         for (Provider prov : new ProviderBD().listaProvider()) {
             if (prov.getNome().equals("OpenStack")) {
                 System.out.println("openstak comentada, quando tiver provedor é so descomentar");
-                executor.execute(new ThreadOpenStack(prov));
+//                executor.execute(new ThreadOpenStack(prov));
             } else if (prov.getNome().equals("Amazon")) {
-//                executor.execute(new ThreadAmazon(prov));
+                executor.execute(new ThreadAmazon(prov));
             }
         }
-        executor.shutdown();
+//        executor.shutdown();
     }
-    
-    public void stopThread(){
-        executor.shutdownNow();
+
+    public void stopThread() {
+        executor.shutdown();
+        try {
+             if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                executor.shutdownNow(); // Cancel currently executing tasks
+                if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                    System.err.println("Pool did not terminate");
+                }
+            }
+        } catch (InterruptedException ie) {
+            executor.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
     }
 }
