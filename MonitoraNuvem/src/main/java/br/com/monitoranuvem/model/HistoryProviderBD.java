d0602a5fc82c3139199914dd6df872dc8e8b6527
Marcio <Marcio@Marcio-PC>
2014-10-15 21:43:50 -0300

    teste

--- MonitoraNuvem/src/main/java/br/com/monitoranuvem/model/HistoryProviderBD.java
+++ MonitoraNuvem/src/main/java/br/com/monitoranuvem/model/HistoryProviderBD.java
@@ -9,7 +9,10 @@ import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HistoryProviderBD {
 
@@ -200,5 +203,110 @@ public class HistoryProviderBD {
             }
         }
         return hltd;
+    }
+
+    //Jogada do mestre
+    public ArrayList<String> montaHistorico(int numDias) throws ClassNotFoundException, SQLException, ParseException {
+        Map<String, String> statusProvider = new HashMap<String, String>();
+        ArrayList<HistoricoInterna> historico = new ArrayList<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String dateForMySql = "";
+        Date data = new Date();
+        dateForMySql = sdf.format(data);
+        conn = new ConnectionMySql().getConnection();
+        PreparedStatement stmt = conn.prepareStatement(""
+                + "SELECT DATE(DATAATUALIZACAO) AS DATA, STATUSPROVIDER, COUNT(*) AS CONTADOR \n"
+                + "FROM HISTORICOPROVIDER "
+                + "WHERE IDHISTORICOPROVIDER IN "
+                + "( SELECT MAX(IDHISTORICOPROVIDER) "
+                + "FROM HISTORICOPROVIDER "
+                + "WHERE DATE(DATAATUALIZACAO) BETWEEN DATE_SUB(?, INTERVAL ? DAY) AND ? "
+                + "GROUP BY DATE(DATAATUALIZACAO),IDINSTANCEPROVIDER) "
+                + "GROUP BY DATE(DATAATUALIZACAO), STATUSPROVIDER "
+                + "ORDER BY DATE(DATAATUALIZACAO) ");
+        stmt.setString(1, dateForMySql);
+        stmt.setInt(2, numDias);
+        stmt.setString(3, dateForMySql);
+        ResultSet resultado = stmt.executeQuery();
+        while (resultado.next()) {
+            statusProvider.put(resultado.getString("STATUSPROVIDER"), "");
+            historico.add(new HistoricoInterna(resultado.getString("DATA"), resultado.getString("STATUSPROVIDER"), resultado.getInt("CONTADOR")));
+        }
+        conn.close();
+        return montaGrafico(statusProvider, historico, numDias);
+    }
+
+    private ArrayList<String> montaGrafico(Map<String, String> status, ArrayList<HistoricoInterna> hist, int numDias) throws ParseException {
+        int count = 0;
+        ArrayList<String> grafico = new ArrayList<>();
+        String hltd = "";
+        hltd += "{";
+        String linha = "";
+        SimpleDateFormat dt;
+        dt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date;
+        ArrayList<String> arDatas = geraDatas(numDias);
+        for (String key : status.keySet()) {
+            hltd += "label: \"" + key + "\",";
+            hltd += "data: [";
+            for (int i = 0; i < arDatas.size(); i++) {
+                date = dt.parse(arDatas.get(i) + " 00:00:00");
+                linha = "[" + date.getTime() + "," + "0" + "]";
+                for (int j = 0; j < hist.size(); j++) {
+                    if (arDatas.get(i).equals(hist.get(j).data)
+                            && key.equals(hist.get(j).status)) {
+                        date = dt.parse(hist.get(j).data + " 00:00:00");
+                        linha = "[" + date.getTime() + "," + hist.get(j).quantidade + "]";
+                        break;
+                    }
+                }
+                hltd += linha;
+                if (i < arDatas.size() - 1) {
+                    hltd += ",";
+                }
+            }
+            hltd += "],";
+            hltd += "lines: {fillColor: \"#f3faff\"},";
+            hltd += "points: {fillColor: \"#fff\"}";
+            hltd += "}";
+            if (count < status.size() - 1) {
+                hltd += ";{";
+            }
+            count++;
+        }
+        date = dt.parse(arDatas.get(arDatas.size() - 1) + " 00:00:00");
+        grafico.add(String.valueOf(date.getTime()));
+        date = dt.parse(arDatas.get(0) + " 00:00:00");
+        grafico.add(String.valueOf(date.getTime()));
+        grafico.add(hltd);
+        return grafico;
+    }
+
+    private ArrayList<String> geraDatas(int numDias) {
+        ArrayList<String> arrDatas = new ArrayList<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String dateForMySql = "";
+        Date data = new Date();
+        dateForMySql = sdf.format(data);
+        Calendar calendar = Calendar.getInstance();
+        for (int i = 0; i <= numDias; i++) {
+            calendar.setTime(data);
+            calendar.add(Calendar.DAY_OF_MONTH, -i);
+            arrDatas.add(sdf.format(calendar.getTime()));
+        }
+        return arrDatas;
+    }
+
+    class HistoricoInterna {
+
+        private final String data;
+        private final String status;
+        private final int quantidade;
+
+        public HistoricoInterna(String data, String status, int quantidade) {
+            this.data = data;
+            this.status = status;
+            this.quantidade = quantidade;
+        }
     }
 }
