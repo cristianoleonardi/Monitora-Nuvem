ddb552d6f0e387e2de2600d97fdba48afe76827f
Marcio <Marcio@Marcio-PC>
2014-10-18 16:45:32 -0300

    teste

--- MonitoraNuvem/src/main/java/br/com/monitoranuvem/model/InstanceProviderBD.java
+++ MonitoraNuvem/src/main/java/br/com/monitoranuvem/model/InstanceProviderBD.java
@@ -438,7 +438,7 @@ public class InstanceProviderBD {
                     if (Integer.parseInt(providerAux.get(keyprovider)) > 0) {
                         dadosGrafico += ",";
                     }
-                    dadosGrafico += "[" + providerAux.get(keyprovider) + "," + 0.1 + "]";
+                    dadosGrafico += "[" + providerAux.get(keyprovider) + "," + 0.2 + "]";
                 }
             }
             if (count1 < statusProvider.size() - 1) {
